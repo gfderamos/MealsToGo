49d00384e5bd13c9ea019f13e52155ded7aaa576
Guinther de Ramos <frank.guinther@gmail.com>
2022-09-15 10:05:20 +0200

    Finished first part, started mock location

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { Text } from "react-native"
 import { ThemeProvider } from "styled-components/native"
 import { NavigationContainer } from "@react-navigation/native"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { Ionicons } from "@expo/vector-icons"
 
 import {
   useFonts as useOswald,
@@ -17,6 +18,12 @@ import { SafeArea } from "./src/components/utility/safe-area.component"
 
 const Tab = createBottomTabNavigator()
 
+const TAB_ICON = {
+  Restaurants: "md-restaurant",
+  Map: "md-map",
+  Settings: "md-settings"
+}
+
 const Settings = () => (
   <SafeArea>
     <Text>Settings</Text>
@@ -28,6 +35,15 @@ const Map = () => (
     <Text>Map</Text>
   </SafeArea>
 )
+
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name]
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    )
+  }
+}
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -46,7 +62,12 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-          <Tab.Navigator>
+          <Tab.Navigator
+            screenOptions={createScreenOptions}
+            tabBarOptions={{
+              activeTintColor: "tomato",
+              inactiveTintColor: "gray"
+            }}>
             <Tab.Screen name='Restaurants' component={RestaurantsScreen} />
             <Tab.Screen name='Map' component={Map} />
             <Tab.Screen name='Settings' component={Settings} />
