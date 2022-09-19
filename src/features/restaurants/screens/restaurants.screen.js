515a44c731aca3e8652204769ed14cfa32271a12
Guinther de Ramos <frank.guinther@gmail.com>
2022-09-19 15:01:13 +0200

    Uptades to code

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -7,6 +7,8 @@ import { SafeArea } from "../../../components/utility/safe-area.component"
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component"
 import { Spacer } from "../../../components/spacer/spacer.component"
 
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context"
+
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `
@@ -17,34 +19,22 @@ const RestaurantList = styled(FlatList).attrs({
   }
 })``
 
-export const RestaurantsScreen = () => (
-  <SafeArea>
-    <SearchContainer>
-      <Searchbar />
-    </SearchContainer>
-    <RestaurantList
-      data={[
-        { name: 1 },
-        { name: 2 },
-        { name: 3 },
-        { name: 4 },
-        { name: 5 },
-        { name: 6 },
-        { name: 7 },
-        { name: 8 },
-        { name: 9 },
-        { name: 10 },
-        { name: 11 },
-        { name: 12 },
-        { name: 13 },
-        { name: 14 }
-      ]}
-      renderItem={() => (
-        <Spacer position='bottom' size='large'>
-          <RestaurantInfoCard />
-        </Spacer>
-      )}
-      keyExtractor={(item) => item.name}
-    />
-  </SafeArea>
-)
+export const RestaurantsScreen = () => {
+  const RestaurantContext = useContext(RestaurantsContext)
+  return (
+    <SafeArea>
+      <SearchContainer>
+        <Searchbar />
+      </SearchContainer>
+      <RestaurantList
+        data={RestaurantContext.restaurants}
+        renderItem={() => (
+          <Spacer position='bottom' size='large'>
+            <RestaurantInfoCard />
+          </Spacer>
+        )}
+        keyExtractor={(item) => item.name}
+      />
+    </SafeArea>
+  )
+}
