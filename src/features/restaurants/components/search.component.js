0c8b235b7e3bc2e1dc429fe246d8196bc1b5c4db
Guinther de Ramos <frank.guinther@gmail.com>
2023-01-09 17:23:55 +0100

    Added location services/mock

---
+++ src/features/restaurants/components/search.component.js
@@ -0,0 +1,19 @@
+import React, { useContext } from "react"
+import styled from "styled-components/native"
+import { Searchbar } from "react-native-paper"
+import { LocationContext } from "../../../services/location/location.context"
+
+import { LocationContext } from "../../../services/location/location.context"
+
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`
+
+export const Search = () => {
+  const locationContext = useContext(LocationContext)
+  return (
+    <SearchContainer>
+      <Searchbar />
+    </SearchContainer>
+  )
+}
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,18 +1,16 @@
 import React, { useContext } from "react"
-import { Searchbar } from "react-native-paper"
+
 import { FlatList } from "react-native"
 import styled from "styled-components/native"
 import { ActivityIndicator, Colors } from "react-native-paper"
 
 import { SafeArea } from "../../../components/utility/safe-area.component"
-import { RestaurantInfoCard } from "../components/restaurant-info-card.component"
 import { Spacer } from "../../../components/spacer/spacer.component"
 
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context"
 
-const SearchContainer = styled.View`
-  padding: ${(props) => props.theme.space[3]};
-`
+import { Search } from "../components/search.component"
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component"
 
 const RestaurantList = styled(FlatList).attrs({
   contentContainerStyle: {
@@ -39,9 +37,7 @@ export const RestaurantsScreen = () => {
           <Loading size={50} animating={true} color={Colors.blue300} />
         </LoadingContainer>
       )}
-      <SearchContainer>
-        <Searchbar />
-      </SearchContainer>
+      <Search />
       <RestaurantList
         data={restaurants}
         renderItem={({ item }) => {
